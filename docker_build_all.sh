64567530af450e871d5b1f931d7a3ca17012328e
gbohner <gbohner@gatsby.ucl.ac.uk>
2019-04-01 18:21:02 +0000

    minor bugfix

--- docker_build_all.sh
+++ docker_build_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Build all stages successively, and also save build log to file for examination later
 docker build -t safehaven_ds:stage1 \
-	--build_arg NB_GID=$(cat users.group.gid) \
-	--build_arg NB_GNAME=$(cat users.group.name) \
+	--build-arg NB_GID=$(cat users.group.gid) \
+	--build-arg NB_GNAME=$(cat users.group.name) \
  	-f Dockerfile.stage1 . 2>&1 | tee build.log
 docker build -t safehaven_ds:stage2 -f Dockerfile.stage2 . 2>&1 | tee -a build.log
 docker build -t safehaven_ds:test_user_auth -f Dockerfile.createusers . 2>&1 | tee -a build.log
